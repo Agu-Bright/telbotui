711b1dd75762b16d44fb00d29d3e28a02f6dc132
Bright <brightagu2@gmail.com>
2024-07-07 18:40:35 +0100

    updae

--- app/page.js
+++ app/page.js
@@ -1,6 +1,19 @@
-import Image from "next/image";
+"use client";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [seeds, setSeeds] = useState();
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get("/");
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
